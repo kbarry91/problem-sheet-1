92be2e96034aafa1a6b50e1302b2a8b30f3fe10d
Kevin Barry <g00339811@gmit.ie>
2017-09-21 18:41:31 +0100

    Problem 2 completed

---
+++ 02-current-time.go
@@ -0,0 +1,14 @@
+//  Author: Kevin Barry
+//  Date:   21/09/2017
+//  Adapted from: https://tour.golang.org/welcome/4
+
+package main
+
+import (
+	"fmt" //import package for print
+	"time" //import package for system time
+)
+
+func main() {
+	fmt.Println("The current date and time is", time.Now())//prints time to console
+}
