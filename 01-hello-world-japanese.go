f5a2fb1c3258ace21e69fcd872c7adafae4cdd73
Kevin Barry <g00339811@gmit.ie>
2017-09-21 22:47:46 +0100

    Problem 1 completed

---
+++ 01-hello-world-japanese.go
@@ -0,0 +1,12 @@
+//  Author: Kevin Barry
+//  Date:   21/09/2017
+//  Adapted from: https://tour.golang.org/welcome/1
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("English : Hello World")
+	fmt.Println("Japanese: こんにちは世界")
+}
