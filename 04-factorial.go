89d355b028c4213150ab683588a9d74b16927567
Kevin Barry <g00339811@gmit.ie>
2017-10-02 18:26:49 +0100

    problem 4

---
+++ 04-factorial.go
@@ -0,0 +1,43 @@
+/*
+	Author	: Kevin Barry
+	Date	: 22/09/2017
+	Program	: Determine the largest and smallest int from a list
+*/
+package main
+
+import (
+	"fmt"      // Import for input/output
+	"math/big" // Import for math
+	"strconv"  // import to convert string
+)
+
+func digitSum(userinput int) int {
+
+	factorial := big.NewInt(1) 
+
+	// calculate factorial
+	for i := 1; i <= userinput; i++ {
+		factorial.Mul(factorial, big.NewInt(int64(i))) 
+	}
+	fmt.Println(userinput,"!  : ",factorial)//output factorial
+
+	var factSum, digit int  
+
+	//calculate sum of factorial digits
+	for i := range factorial.String() {
+			digit, _ = strconv.Atoi(string(factorial.String()[i])) 
+			factSum += digit                                 
+	}
+	return factSum
+	
+}//digitSum
+
+func main() {
+	//take user input from console
+	var userinput int
+	fmt.Print("Please enter a number(1-100): " )
+	fmt.Scan(&userinput)
+
+	fmt.Println("Input : ",userinput)
+	fmt.Println("Sum of Factorial Digits : ",digitSum(userinput))
+}
